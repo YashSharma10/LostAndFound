e7e22aa5fa7379d26e1c58358245c0fc1b595c82
YashSharma10 <yashsharma67953@gmail.com>
2024-08-16 18:39:17 +0530

    Home Instructions

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -5,6 +5,7 @@ import "./Home.css";
 import lossImage from "../Assets/loss.png";
 import foundImage from "../Assets/found.png";
 import findImage from "../Assets/find.png";
+import { FaExclamationCircle, FaCheckCircle, FaSearch } from 'react-icons/fa';
 
 function Home() {
   return (
@@ -14,23 +15,36 @@ function Home() {
       </div>
       <div className="home">
         <Glass
-          title="Lost an Item"
+          title="Report an Item"
           subtitle="Report"
           imageSrc={lossImage}
           to="/Report"
         />
         <Glass
-          title="Found an Item"
+          title="Browse Found items"
           subtitle="Report"
           imageSrc={foundImage}
-          to="/Report"
+          to="/founditm"
         />
         <Glass
-          title="Find Your Lost Item"
+          title="Browse Lost Items"
           subtitle="Browse"
           imageSrc={findImage}
           to="/Lostitm"
         />
+      </div>
+      <div className="instructions">
+        <h2>How to Use the Lost and Found Website</h2>
+        <div className="instruction-card">
+          <FaExclamationCircle className="instruction-icon" />
+          <h3>Report a Item</h3>
+          <p>If you've lost an item or found any item, click on "Report an Item" to report it. Provide a detailed description and upload an image if available to help others identify it.</p>
+        </div>
+        <div className="instruction-card">
+          <FaSearch className="instruction-icon" />
+          <h3>Browse Items</h3>
+          <p>Click on "Lost Item" or "Found Item"to search through the list of lost and items. Use filters or keywords to quickly locate the item you're looking for.</p>
+        </div>
       </div>
     </>
   );
