938ff031c278e2fed130635b32fcb7b5c52f5cbe
YashSharma10 <yashsharma67953@gmail.com>
2024-08-22 10:53:28 +0530

    aa

--- frontend/src/context/GlobalContextProvider.jsx
+++ frontend/src/context/GlobalContextProvider.jsx
@@ -3,7 +3,7 @@ import { createContext, useContext } from "react";
 export const GlobalContext = createContext();
 
 export default function GlobalContextProvider({ children }) {
-  const globalBackendUrl = "https://lost-and-found-cyan.vercel.app/";
+  const globalBackendUrl = "https://lost-and-found-cyan.vercel.app";
   return (
     <GlobalContext.Provider value={{ globalBackendUrl }}>
       {children}
