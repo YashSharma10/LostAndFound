b61ca2635a3c65d733f946d6c092155957a45d9f
YashSharma10 <yashsharma67953@gmail.com>
2024-08-23 10:17:14 +0530

    deploy

--- frontend/src/context/GlobalContextProvider.jsx
+++ frontend/src/context/GlobalContextProvider.jsx
@@ -3,7 +3,7 @@ import { createContext, useContext } from "react";
 export const GlobalContext = createContext();
 
 export default function GlobalContextProvider({ children }) {
-  const globalBackendUrl = "http://localhost:6005";
+  const globalBackendUrl = "https://lostandfound-40ek.onrender.com";
   return (
     <GlobalContext.Provider value={{ globalBackendUrl }}>
       {children}
