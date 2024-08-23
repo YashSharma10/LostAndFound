7af88e8e5e529413a5912edc7239d6069acf98a7
YashSharma10 <yashsharma67953@gmail.com>
2024-08-23 12:02:50 +0530

    solve

--- backend/app.js
+++ backend/app.js
@@ -1,43 +1,44 @@
-import express from 'express';
-import cors from 'cors';
-import session from 'express-session';
-import passport from 'passport';
-import connectMongo from 'connect-mongo';
-import FoundItemsRoutes from './routes/foundItem.js';
-import LostItemsRoutes  from './routes/lostItem.js';
-import User from './models/User.js';
-import { Strategy as OAuth2Strategy } from 'passport-google-oauth20';
-
+import express from "express";
+import cors from "cors";
+import session from "express-session";
+import passport from "passport";
+import connectMongo from "connect-mongo";
+import FoundItemsRoutes from "./routes/foundItem.js";
+import LostItemsRoutes from "./routes/lostItem.js";
+import User from "./models/User.js";
+import { Strategy as OAuth2Strategy } from "passport-google-oauth20";
 
 const app = express();
 const PORT = 6005;
 
-const clientid = "1054288691399-pjasu3r6f77sugpr1ff1n70gg4tpd5hh.apps.googleusercontent.com";
+const clientid =
+  "1054288691399-pjasu3r6f77sugpr1ff1n70gg4tpd5hh.apps.googleusercontent.com";
 const clientsecret = "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";
 
 // Create MongoStore instance
 const MongoStore = connectMongo.create({
-  mongoUrl: 'mongodb+srv://yash22csu295:12345@lostandfound.wgyek.mongodb.net/?retryWrites=true&w=majority&appName=LostAndFound',
-  collectionName: 'sessions',
+  mongoUrl:
+    "mongodb+srv://yash22csu295:12345@lostandfound.wgyek.mongodb.net/?retryWrites=true&w=majority&appName=LostAndFound",
+  collectionName: "sessions",
 });
 
 app.use(
   cors({
-    origin: 'https://lostandfound-1.onrender.com',
+    origin: "https://lostandfound-1.onrender.com",
     credentials: true,
   })
 );
 
 const globalURL = "https://lostandfound-1.onrender.com";
 
-app.use(express.json({ limit: '16kb' }));
-app.use(express.urlencoded({ extended: true, limit: '16kb' }));
-app.use(express.static('public'));
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
 
 // Setup session middleware
 app.use(
   session({
-    secret: 'secretekey',
+    secret: "secretekey",
     resave: false,
     saveUninitialized: true,
     store: MongoStore,
@@ -58,8 +59,8 @@ passport.use(
     {
       clientID: clientid,
       clientSecret: clientsecret,
-      callbackURL: '/auth/google/callback',
-      scope: ['profile', 'email'],
+      callbackURL: "https://lostandfound-1.onrender.com/auth/google/callback",
+      scope: ["profile", "email"],
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
@@ -93,22 +94,28 @@ passport.deserializeUser((user, done) => {
 });
 
 // Initial Google OAuth login
-app.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+app.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
 
-app.get('/auth/google/callback', passport.authenticate('google', {
-  successRedirect: `${globalURL}`,
-  failureRedirect: `${globalURL}/login`,
-}));
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", {
+    successRedirect: `${globalURL}`,
+    failureRedirect: `${globalURL}/login`,
+  })
+);
 
-app.get('/login/success', async (req, res) => {
+app.get("/login/success", async (req, res) => {
   if (req.user) {
-    res.status(200).json({ message: 'user Login', user: req.user });
+    res.status(200).json({ message: "user Login", user: req.user });
   } else {
-    res.status(400).json({ message: 'Not Authorized' });
+    res.status(400).json({ message: "Not Authorized" });
   }
 });
 
-app.get('/logout', (req, res, next) => {
+app.get("/logout", (req, res, next) => {
   req.logout(function (err) {
     if (err) {
       return next(err);
@@ -116,7 +123,7 @@ app.get('/logout', (req, res, next) => {
     res.redirect(`${globalURL}`);
   });
 });
-app.get('/auth/status', (req, res) => {
+app.get("/auth/status", (req, res) => {
   if (req.isAuthenticated()) {
     res.json({ authenticated: true, user: req.user });
   } else {
@@ -124,10 +131,8 @@ app.get('/auth/status', (req, res) => {
   }
 });
 
-
-
 // Use routes
-app.use('/api/reports/lost', LostItemsRoutes);
-app.use('/api/reports/found', FoundItemsRoutes);
+app.use("/api/reports/lost", LostItemsRoutes);
+app.use("/api/reports/found", FoundItemsRoutes);
 
-export {app}
+export { app };
