3a4d595c8f36fb0a13ed366e91b865770cddfc07
YashSharma10 <yashsharma67953@gmail.com>
2024-08-28 10:34:23 +0530

    ..

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,7 +7,7 @@ import GlobalContextProvider from "./context/GlobalContextProvider.jsx";
 // import AuthProvider from "./context/AuthProvider.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <GoogleOAuthProvider clientId="957033742281-1rd2kunk0u0dmj4h822l7mf17bdc9go1.apps.googleusercontent.com">
+  <GoogleOAuthProvider clientId="1054288691399-pjasu3r6f77sugpr1ff1n70gg4tpd5hh.apps.googleusercontent.com">
     <GlobalContextProvider>
       <App />
     </GlobalContextProvider>
